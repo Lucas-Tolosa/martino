657b2f8e751943aeb981bcbb8863664389b86f9a
Lucas-Tolosa <tolosaalucas@gmail.com>
2026-02-01 11:15:47 -0300

    new Modality

--- archivos.js
+++ archivos.js
@@ -1,14 +1,30 @@
-var productos = document.createElement("div");
-function productos(){
 
+function productos(){
+var productos = document.createElement("div");
+productos.className=("container1");
 fetch("http://localhost:8080/Productos/todos").then(Response=>Response.text().then(texto => 
-{   var txt = JSON.parse(texto);
+{   const txt = JSON.parse(texto);
   //  productos.innerHTML= sk;
   
-    console.log(txt);
     
-//document.body.appendChild(productos);
+    console.log(txt.length);
+    for(let x =0; x<txt.length;x++){
+        productos.innerHTML += (` <div class="caja" id="${txt[x].nombre}">${txt[x].nombre}
+                <button onclick="agregar('${txt[x].nombre}')" class="btn-agregar" >agregar</button>
+                <p>Stock: ${txt[x].stock} </p>
+            </div>`);
+    }
+    console.log(txt[0].nombre);
+    
+document.body.appendChild(productos);
+}
+).catch(error => console.error('Error:',error)));
+var btnVaciar = document.createElement("div");
+btnVaciar.innerHTML=`</div>
+        <button type="button" onclick="vaciar()">vaciar canasto</button>
+        <div id="carrito">
+        </div>`;
+document.body.appendChild(btnVaciar);
+
 }
 
-).catch(error => console.error('Error:',error)));
-}
--- martino (1)/martino/src/main/java/com/ejercicios/martino/carrito/productosController.java
+++ martino (1)/martino/src/main/java/com/ejercicios/martino/carrito/productosController.java
@@ -1,22 +1,20 @@
 package com.ejercicios.martino.carrito;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/Productos")
+@CrossOrigin("*")
 public class productosController {
-    private Productos [] p1 = new Productos[6];
+    private Productos [] p1 = new Productos[5];
 //definimos cada producto harcodeado, en un futuro lo juntamos con la base de datos
     public productosController(){
-        p1[0] = new Productos("Balcarce","Torta de pionono relleno de ddl",30000,2);
-        p1[1] = new Productos("Ricota","Torta de ricota con masa",22000,4);
-        p1[2] = new Productos("Pan","Pan frances, pueden ser flautas o miñones",4000,40);
-        p1[3] = new Productos("Huevo","Es un huevo no molestes",300,30);
-        p1[4] = new Productos("Polenta","buenasa con quesito y salsa bolognesa",1800,1);
+        p1[0] = new Productos("balcarce","Torta de pionono relleno de ddl",30000,2);
+        p1[1] = new Productos("ricota","Torta de ricota con masa",22000,4);
+        p1[2] = new Productos("pan","Pan frances, pueden ser flautas o miñones",4000,40);
+        p1[3] = new Productos("huevo","Es un huevo no molestes",300,30);
+        p1[4] = new Productos("polenta","buenasa con quesito y salsa bolognesa",1800,1);
     }
     @GetMapping("/todos")
     public Productos[] getProductos(){
